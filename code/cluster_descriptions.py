a79806262e5284af0ee692fa7a68f2f2631d978a
Robert Leist <robert.leist@dfki.de>
2023-06-15 09:01:44 +0200

    removed n clusters loop

--- code/cluster_descriptions.py
+++ code/cluster_descriptions.py
@@ -18,18 +18,17 @@ n_clusters = int(np.floor(np.sqrt(len(train_descriptions)))) + 4
 
 pca = fit_kernel_pca(data=train_embeddings, dimensions=3)
 
-for n_clusters in range(len(train_descriptions) - 5, 5, -5):
-    clustering = cluster_data(data=train_embeddings, pca=pca, n_clusters=n_clusters, n_init=50,
-                                               init="random", random_state=69)
+clustering = cluster_data(data=train_embeddings, pca=pca, n_clusters=n_clusters, n_init=50,
+                                           init="random", random_state=69)
 
-    train_clusters, train_projected = predict_cluster(data=train_embeddings, pca=pca, clustering=clustering)
-    test_clusters, test_projected = predict_cluster(data=test_embeddings, pca=pca, clustering=clustering)
+train_clusters, train_projected = predict_cluster(data=train_embeddings, pca=pca, clustering=clustering)
+test_clusters, test_projected = predict_cluster(data=test_embeddings, pca=pca, clustering=clustering)
 
-    print_clusters(train_labels, train_clusters)
+print_clusters(train_labels, train_clusters)
 
-    visualize_clustering(train_clusters=train_clusters, train_labels=train_labels, train_projected=train_projected,
-                         test_clusters=test_clusters, test_labels=test_labels, test_projected=test_projected,
-                         fontsize=8, cmap="rainbow", dimensions=2)
+visualize_clustering(train_clusters=train_clusters, train_labels=train_labels, train_projected=train_projected,
+                     test_clusters=test_clusters, test_labels=test_labels, test_projected=test_projected,
+                     fontsize=8, cmap="rainbow", dimensions=3)
 
-#visualize_tsne(data=train_embeddings+test_embeddings, labels=train_labels+test_labels, dimensions=2,
-#            perplexity=np.sqrt(len(train_embeddings)+len(test_embeddings)), fontsize=5, random_state=69)
+visualize_tsne(data=train_embeddings+test_embeddings, labels=train_labels+test_labels, dimensions=2,
+            perplexity=np.sqrt(len(train_embeddings)+len(test_embeddings)), fontsize=5, random_state=69)
