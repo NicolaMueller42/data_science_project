89e1e702f21b3dce28ef90b8bd93f8527a91c1b0
Robert Leist <robert.leist@dfki.de>
2023-06-19 11:09:52 +0200

    added paths

--- paths.py
+++ paths.py
@@ -3,3 +3,6 @@ from os.path import join
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 DATA = join(ROOT, "data")
+TEST_CSV = join(DATA, "test.csv")
+TRAIN_CSV = join(DATA, "train.csv")
+ECO_CSV = join(DATA, "economic_data.csv")
